2df71f9900c03daf155aa2eb8f97a2a06a89afad
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-07-31 09:55:56 +1000

    fixed main file code

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# sitemap-generator
-sitemap generator for bulk pages created via htaccess redirection
-Access it via url to trigger it
+# Sitemap Generator
+
+Let's say you want to create a specific sitemap with just some urls. For example:
+Instead of creating a sitemap with all the pages via Screaming Frog or Yoast, you can:
+
+> Download this folder and upload on your website root
+> Insert all the pages you want in the pages.txt
+> Enter on the url your-site.com/sitemap-generator/sitemap-generator.php
+> The Sitemap will be automatically there.
--- sitemap-generator.php
+++ sitemap-generator.php
@@ -1,21 +1,32 @@
-<?php 
-$keywords = [
-    'keyword1',
-    'keyword2',
-    'keyword3'
-];
+<?php
+// Read keywords from the file and store them in an array
+$keywords = file('pages.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
 // Start building the sitemap
 $sitemap = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
-$sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL; // Loop through the array and add each URL to the sitemap
+$sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
+
+// Loop through the array and add each URL to the sitemap
 foreach ($keywords as $keyword) {
-    $url = 'https://examplewebsite.com.au/prefix-before-' . strtolower(str_replace(' ', '-', $keyword));
-    $sitemap .= '<url>' . PHP_EOL;
-    $sitemap .= '<loc>' . $url . '</loc>' . PHP_EOL;
-    $sitemap .= '<lastmod>' . date('Y-m-d') . '</lastmod>' . PHP_EOL;
-    $sitemap .= '<changefreq>daily</changefreq>' . PHP_EOL;
-    $sitemap .= '<priority>0.8</priority>' . PHP_EOL;
-    $sitemap .= '</url>' . PHP_EOL;
-} // Finish building the sitemap
-$sitemap .= '</urlset>' . PHP_EOL; // Output the sitemap to the browser or save to a file
+    // Clean up the keyword for the URL
+    $cleanKeyword = strtolower(str_replace(' ', '-', trim($keyword)));
+
+    // Construct the URL using the cleaned keyword
+    $url = $cleanKeyword;
+
+    // Add the URL and other sitemap attributes
+    $sitemap .= '<url>' . PHP_EOL;
+    $sitemap .= '<loc>' . htmlspecialchars($url) . '</loc>' . PHP_EOL;
+    $sitemap .= '<lastmod>' . date('Y-m-d') . '</lastmod>' . PHP_EOL;
+    $sitemap .= '<changefreq>daily</changefreq>' . PHP_EOL;
+    $sitemap .= '<priority>0.8</priority>' . PHP_EOL;
+    $sitemap .= '</url>' . PHP_EOL;
+}
+
+// Finish building the sitemap
+$sitemap .= '</urlset>' . PHP_EOL;
+
+// Output the sitemap to the browser or save to a file
 header('Content-Type: application/xml');
 echo $sitemap;
+?>
